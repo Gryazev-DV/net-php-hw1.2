ac2800c0a5b88e3d8c14a113cfe939abb1c6bc11
Dmitry Gryazev <gryazevdv@yandex.ru>
2017-08-14 19:05:21 +0300

    правильная версия файла

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
 
-$x = rand(0,100);
-echo "Число ".$x;
+// $x = $_GET['x'];
+$x = rand(0, 100);
+echo "Число x = " . $x . ".";
+echo PHP_EOL;
 
-$a=1;
-$b=1;
+$a = 1;
+$b = 1;
 
 while(true) {
   if ($a > $x) {
   	echo "Задуманное число не входит в числовой ряд";
   	break;
-  } elseif ($a = $x) {
+  } elseif ($a == $x) {
   	echo "Задуманное число входит в числовой ряд";
   	break;
   } else {
   	$c = $a;
-  	$a += $b;
+  	$a = $a + $b;
   	$b = $c;
   }
 }
 
-echo "Число b".$b
 
 
 ?>
